0b600685dea829415884ba6432ca69acf41d5f13
yixiao17 <yixiao17@vt.edu>
2021-12-09 14:13:20 -0500

    Update dataclean_1.py

--- spark/dataclean_1.py
+++ spark/dataclean_1.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     data_full = data_full.mapPartitions(lambda x: reader(x))
     data_full = data_full.filter(lambda x: x[0] != "Project ID")
 
-    data = data_full.map(lambda x: (x[1], x[5], x[6], x[8]).sortByKey()
+    data = data_full.map(lambda x: (x[1], x[5], x[6], x[8])).sortByKey()
     data_result = data.map(lambda x: str(x[0]) + "\t" + str(x[1]) + " " + str(x[2]) + "\t" + str(x[3]))
 
     data_result.saveAsTextFile("data1.out")
