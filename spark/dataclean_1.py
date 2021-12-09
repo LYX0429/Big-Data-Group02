a7bf6dce23d20cb3c0ea73e7d4dcb441e20e9a23
yixiao17 <yixiao17@vt.edu>
2021-12-09 14:38:10 -0500

    data1

--- spark/dataclean_1.py
+++ spark/dataclean_1.py
@@ -3,15 +3,14 @@ from operator import add
 from pyspark import SparkContext
 from csv import reader
 
-if __name__ == "__main__":
-    sc = SparkContext()
-    fileName = "/user/CS-GY-6513/project_data/data-cityofnewyork-us.hg8x-zxpr.csv"
-    data_full = sc.textFile(fileName, 1)
-    data_full = data_full.mapPartitions(lambda x: reader(x))
-    data_full = data_full.filter(lambda x: x[0] != "Project ID")
+sc = SparkContext()
+fileName = "/user/CS-GY-6513/project_data/data-cityofnewyork-us.hg8x-zxpr.csv"
+data_full = sc.textFile(fileName, 1)
+data_full = data_full.mapPartitions(lambda x: reader(x))
+data_full = data_full.filter(lambda x: x[0] != "Project ID")
 
-    data = data_full.map(lambda x: (x[1], x[5], x[6], x[8])).sortByKey()
-    data_result = data.map(lambda x: str(x[0]) + "\t" + str(x[1]) + " " + str(x[2]) + "\t" + str(x[3]))
+data = data_full.map(lambda x: (x[1], x[5], x[6], x[8])).sortByKey()
+data_result = data.map(lambda x: str(x[0]) + "\t" + str(x[1]) + " " + str(x[2]) + "\t" + str(x[3]))
 
-    data_result.saveAsTextFile("data1.out")
-    sc.stop()
+data_result.saveAsTextFile("data1.tsv")
+sc.stop()
