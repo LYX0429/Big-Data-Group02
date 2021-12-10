3969a424dc476dc1de888892982208be4b24e37a
yixiao17 <yixiao17@vt.edu>
2021-12-10 15:41:13 -0500

    Update dataclean_7.py

--- spark/dataclean_7.py
+++ spark/dataclean_7.py
@@ -9,7 +9,7 @@ data_full = sc.textFile(fileName, 1)
 data_full = data_full.mapPartitions(lambda x: reader(x))
 data_full = data_full.filter(lambda x: x[0] != "ISN_DOB_BIS_VIOL")
 
-data = data_full.map(lambda x: (x[7] + " " + x[8], x[5], x[6], x[15], x[16])).sort(lambda x: x[0])
+data = data_full.map(lambda x: (x[7] + " " + x[8], x[5], x[6], x[15], x[16])).sortBy(lambda x: x[0])
 data_result = data.map(lambda x: str(x[0]) + "\t" + str(x[1]) + "\t" + str(x[2]) + "\t" + str(x[3]) + "\t" + str(x[4]))
 
 data_result.saveAsTextFile("data7.tsv")
