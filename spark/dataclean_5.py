29b165c4792cedd747d5d9362b040b76aab29811
yixiao17 <yixiao17@vt.edu>
2021-12-10 15:08:42 -0500

    Update dataclean_5.py

--- spark/dataclean_5.py
+++ spark/dataclean_5.py
@@ -4,16 +4,13 @@ from pyspark import SparkContext
 from csv import reader
 
 sc = SparkContext()
-fileName = "/user/CS-GY-6513/project_data/data-cityofnewyork-us.xi7c-iiu2.csv"
+fileName = "/user/CS-GY-6513/project_data/data-cityofnewyork-us.8isn-pgv3.csv"
 data_full = sc.textFile(fileName, 1)
 data_full = data_full.mapPartitions(lambda x: reader(x))
-data_full = data_full.filter(lambda x: x[0] != "Borough")
+data_full = data_full.filter(lambda x: x[3] != "Borough")
 
-data = data_full.map(lambda x: (x[0], x[4], x[5], x[6], x[7])).sortByKey()
-#data = data.filter(lambda x: x[0] != "CONFIDENTIAL")
-data = data.toDF().dropna(how='any')
-data = data.rdd
-data_result = data.map(lambda x: str(x[0]) + "\t" + str(x[1]) + "\t" + str(x[2]) + "\t" + str(x[3]) + "\t" + str(x[4]))
+data = data_full.map(lambda x: (x[1], x[2], x[19], x[20], x[21], x[22])).sortByKey()
+data_result = data.map(lambda x: str(x[0]) + "\t" + str(x[1]) + "\t" + str(x[2]) + "\t" + str(x[3]) + "\t" + str(x[4]) + "\t" + str(x[5]))
 
-data_result.saveAsTextFile("data4.tsv")
+data_result.saveAsTextFile("data5.tsv")
 sc.stop()
