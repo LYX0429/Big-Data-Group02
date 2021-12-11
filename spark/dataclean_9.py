8256249336e2fc257ce5bfc2af84642957d9e66f
songma <30760291+songmatianqi@users.noreply.github.com>
2021-12-10 22:15:07 -0500

    add dataclean_9

---
+++ spark/dataclean_9.py
@@ -0,0 +1,18 @@
+import sys
+from operator import add
+from pyspark import SparkContext
+from csv import reader
+
+c = SparkContext()
+#Inclusionary Housing Projects
+fileName = "/user/CS-GY-6513/project_data/data-cityofnewyork-us.jafx-rvrb.csv"
+data_full = sc.textFile(fileName, 1)
+data_full = data_full.mapPartitions(lambda x: reader(x))
+data_full = data_full.filter(lambda x: x[0] != "Project ID")
+data_full = data_full.filter(lambda x: x[3] == "Completed")
+data_full = data_full.filter(lambda x: x[6] != "0")
+data_full = data_full.filter(lambda x: x[6] != "-2")
+data = data_full.map(lambda x: (x[1], x[6])).sortByKey()
+data_result = data.map(lambda x: str(x[0]) + "\t" + str(x[1]))
+data_result.saveAsTextFile("data9.tsv")
+sc.stop()
